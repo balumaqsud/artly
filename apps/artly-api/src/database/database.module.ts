ac1121c445a1e986f332cfd2bf5d747c36448458
balumaqsud <ollobergan008@mail.ru>
2025-06-14 20:10:38 +0900

    fix: connect mongo db database

--- apps/artly-api/src/database/database.module.ts
+++ apps/artly-api/src/database/database.module.ts
@@ -1,4 +1,34 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule, InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 
-@Module({})
-export class DatabaseModule {}
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri:
+          process.env.NODE_ENV === 'production'
+            ? process.env.MONGO_PROD
+            : process.env.MONGO_DEV,
+      }),
+    }),
+  ],
+  exports: [MongooseModule],
+})
+export class DatabaseModule {
+  constructor(
+    @InjectConnection()
+    private readonly connection: Connection,
+  ) {
+    if (connection.readyState) {
+      console.log(connection.readyState);
+      console.log(
+        `Mongo DB connected to ${
+          process.env.NODE_ENV === 'production' ? 'prod' : 'dev'
+        } mode`,
+      );
+    } else {
+      console.log('Mongo db is not connected');
+    }
+  }
+}
