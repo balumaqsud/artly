e22b5a0a5cf8560c8d5408fe351cd372cc71de8f
balumaqsud <ollobergan008@mail.ru>
2025-06-15 20:55:13 +0900

    feat: developed auth service methods

--- apps/artly-api/src/components/auth/auth.module.ts
+++ apps/artly-api/src/components/auth/auth.module.ts
@@ -1,4 +1,16 @@
+import { HttpModule } from '@nestjs/axios';
+import { AuthService } from './auth.service';
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 
-@Module({})
+@Module({
+  imports: [
+    HttpModule,
+    JwtModule.register({
+      secret: `${process.env.SECRET_KEY}`,
+      signOptions: { expiresIn: '10d' },
+    }),
+  ],
+  providers: [AuthService],
+})
 export class AuthModule {}
---
+++ apps/artly-api/src/components/auth/auth.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcryptjs';
+import { BlobOptions } from 'buffer';
+import { Member } from '../../libs/dto/member/member';
+import { T } from '../../libs/types/common';
+import { shapeId } from '../../libs/config';
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly jwtService: JwtService) {}
+
+  public async hasPassword(memberPassword: string): Promise<string> {
+    const salt = await bcrypt.genSalt();
+    return await bcrypt.hash(memberPassword, salt);
+  }
+
+  public async comparePassword(
+    password: string,
+    hashedPassword: string,
+  ): Promise<boolean> {
+    return await bcrypt.compare(password, hashedPassword);
+  }
+
+  public async createToken(member: Member): Promise<string> {
+    const payload: T = {};
+    //copied
+    Object.keys(member['_doc'] ? member['_doc'] : member).map((ele) => {
+      payload[`${ele}`] = member[`${ele}`];
+    });
+
+    delete payload.memberPassword;
+    return this.jwtService.signAsync(payload);
+  }
+
+  private async verifyToken(token: string): Promise<Member> {
+    const member = await this.jwtService.verifyAsync(token);
+    member._id = shapeId(member._id);
+    return member;
+  }
+}
