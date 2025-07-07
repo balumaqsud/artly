e5985d659afc2437b28054c2eb5acf6d7b4a4b7a
balumaqsud <ollobergan008@mail.ru>
2025-07-07 21:44:18 +0900

    feat: createArticle graphQL api created

--- apps/artly-api/src/components/community/community.resolver.ts
+++ apps/artly-api/src/components/community/community.resolver.ts
@@ -1,7 +1,23 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { CommunityService } from './community.service';
+import { UseGuards } from '@nestjs/common';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { BoardArticle } from '../../libs/dto/board-article/board-article';
+import { BoardArticleInput } from '../../libs/dto/board-article/board-article.input';
+import { AuthMember } from '../auth/decorators/authMember.decorator';
+import { ObjectId } from 'mongoose';
 
 @Resolver()
 export class CommunityResolver {
   constructor(private readonly communityService: CommunityService) {}
+
+  @UseGuards(AuthGuard)
+  @Mutation((returns) => BoardArticle)
+  public async createArticle(
+    @Args('input') input: BoardArticleInput,
+    @AuthMember('_id') memberId: ObjectId,
+  ): Promise<BoardArticle> {
+    console.log('mutation, create article');
+    return await this.communityService.createArticle(input, memberId);
+  }
 }
--- apps/artly-api/src/components/community/community.service.ts
+++ apps/artly-api/src/components/community/community.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, ObjectId } from 'mongoose';
 import { MemberService } from '../member/member.service';
 import { ViewService } from '../view/view.service';
 import { BoardArticle } from '../../libs/dto/board-article/board-article';
+import { BoardArticleInput } from '../../libs/dto/board-article/board-article.input';
+import { Message } from '../../libs/enums/common.enum';
 
 @Injectable()
 export class CommunityService {
@@ -14,4 +16,23 @@ export class CommunityService {
     private viewService: ViewService,
     // private likeService: LikeService,
   ) {}
+
+  public async createArticle(
+    input: BoardArticleInput,
+    memberId: ObjectId,
+  ): Promise<BoardArticle> {
+    input.memberId = memberId;
+    try {
+      const result = await this.communityModel.create(input);
+      await this.memberService.memberStatsEditor({
+        _id: memberId,
+        targetKey: 'memberArticles',
+        modifier: 1,
+      });
+      return result;
+    } catch (error) {
+      console.log('create error', error);
+      throw new BadRequestException(Message.CREATE_FAILED);
+    }
+  }
 }
