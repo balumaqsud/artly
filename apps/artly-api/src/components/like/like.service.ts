c717600e818d082e3260f9ebfae3817301bf7946
balumaqsud <ollobergan008@mail.ru>
2025-07-10 04:53:44 +0900

    feat: like service logic developed

---
+++ apps/artly-api/src/components/like/like.service.ts
@@ -0,0 +1,94 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, ObjectId } from 'mongoose';
+import { Like, MeLiked } from '../../libs/dto/like/like';
+import { LikeInput } from '../../libs/dto/like/like.input';
+import { Message } from '../../libs/enums/common.enum';
+import { OrdinaryInquiry } from '../../libs/dto/product/product.input';
+import { Products } from '../../libs/dto/product/product';
+import { LikeGroup } from '../../libs/enums/like.enum';
+import { T } from '../../libs/types/common';
+import { lookupFavorite } from '../../libs/config';
+
+@Injectable()
+export class LikeService {
+  constructor(@InjectModel('Like') private readonly likeModel: Model<Like>) {}
+
+  public async makeToggle(input: LikeInput): Promise<number> {
+    const search = { memberId: input.memberId, likeRefId: input.likeRefId };
+    const exist = await this.likeModel.findOne(search).exec();
+
+    let modifier = 1;
+    if (exist) {
+      await this.likeModel.findOneAndDelete(search).exec();
+      modifier = -1;
+    } else {
+      try {
+        await this.likeModel.create(input);
+      } catch (error) {
+        console.log('likeToggle err:', error);
+        throw new InternalServerErrorException(Message.BAD_REQUEST);
+      }
+    }
+    console.log(`like modifier: ${modifier}`);
+    return modifier;
+  }
+
+  public async checkLikeExistence(input: LikeInput): Promise<MeLiked[]> {
+    const { memberId, likeRefId } = input;
+    const result = await this.likeModel.findOne({
+      memberId: memberId,
+      likeRefId: likeRefId,
+    });
+    return result
+      ? [
+          {
+            memberId: memberId,
+            likeRefId: likeRefId,
+            myFavorite: true,
+          },
+        ]
+      : [];
+  }
+
+  public async getFavoriteProperties(
+    memberId: ObjectId,
+    input: OrdinaryInquiry,
+  ): Promise<Products> {
+    const { page, limit } = input;
+    const match: T = { likeGroup: LikeGroup.PROPERTY, memberId: memberId };
+
+    const data: T = await this.likeModel
+      .aggregate([
+        { $match: match },
+        { $sort: { createdAt: -1 } },
+        {
+          $lookup: {
+            from: 'products',
+            localField: 'likeRefId',
+            foreignField: '_id',
+            as: 'favoriteProduct',
+          },
+        },
+        { $unwind: '$favoriteProduct' },
+        {
+          $facet: {
+            list: [
+              {
+                $skip: (page - 1) * limit,
+              },
+              { $limit: limit },
+              lookupFavorite,
+              { $unwind: '$favoriteProduct.memberData' },
+            ],
+            metaCounter: [{ $count: 'total' }],
+          },
+        },
+      ])
+      .exec();
+
+    const result: Products = { list: [], metaCounter: data[0].metaCounter };
+    result.list = data[0].list.map((ele) => ele.favoriteProduct);
+    return result;
+  }
+}
