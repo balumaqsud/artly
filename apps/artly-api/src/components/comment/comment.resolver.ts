7b757614e7098d96c52ea974c86e2f1fb59bae60
balumaqsud <ollobergan008@mail.ru>
2025-07-10 04:25:39 +0900

    feat: comments update and getcomments logic

--- apps/artly-api/src/components/comment/comment.resolver.ts
+++ apps/artly-api/src/components/comment/comment.resolver.ts
@@ -1,11 +1,16 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CommentService } from './comment.service';
 import { ObjectId } from 'mongoose';
-import { CommentInput } from '../../libs/dto/comment/comment.input';
-import { Comment } from '../../libs/dto/comment/comment';
+import {
+  CommentInput,
+  CommentsInquiry,
+} from '../../libs/dto/comment/comment.input';
+import { Comment, Comments } from '../../libs/dto/comment/comment';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
 import { AuthGuard } from '../auth/guards/auth.guard';
 import { UseGuards } from '@nestjs/common';
+import { CommentUpdate } from '../../libs/dto/comment/comment.update';
+import { shapeId } from '../../libs/config';
 
 @Resolver()
 export class CommentResolver {
@@ -21,5 +26,27 @@ export class CommentResolver {
   ): Promise<Comment> {
     console.log('mutation, create comment');
     return await this.commentService.createComment(memberId, input);
+  }
+
+  @UseGuards(AuthGuard)
+  @Mutation((returns) => Comment)
+  public async updateComment(
+    @Args('input') input: CommentUpdate,
+    @AuthMember('_id') memberId: ObjectId,
+  ): Promise<Comment> {
+    console.log('mutation: updateComment');
+    input._id = shapeId(input._id);
+    return await this.commentService.updateComment(memberId, input);
+  }
+
+  @UseGuards(AuthGuard)
+  @Query((returns) => Comments)
+  public async getComments(
+    @Args('input') input: CommentsInquiry,
+    @AuthMember('_id') memberId: ObjectId,
+  ): Promise<Comments> {
+    console.log('Query: getComments');
+    input.search.commentRefId = shapeId(input.search.commentRefId);
+    return await this.commentService.getComments(memberId, input);
   }
 }
--- apps/artly-api/src/components/comment/comment.service.ts
+++ apps/artly-api/src/components/comment/comment.service.ts
@@ -8,10 +8,17 @@ import { Model, ObjectId } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { ViewService } from '../view/view.service';
 import { ProductService } from '../product/product.service';
-import { CommentInput } from '../../libs/dto/comment/comment.input';
-import { Comment } from '../../libs/dto/comment/comment';
-import { Message } from '../../libs/enums/common.enum';
-import { CommentGroup } from '../../libs/enums/comment.enum';
+import {
+  CommentInput,
+  CommentsInquiry,
+} from '../../libs/dto/comment/comment.input';
+import { Comment, Comments } from '../../libs/dto/comment/comment';
+import { Direction, Message } from '../../libs/enums/common.enum';
+import { CommentGroup, CommentStatus } from '../../libs/enums/comment.enum';
+import { CommentUpdate } from '../../libs/dto/comment/comment.update';
+import { ArticleStatus } from '../../libs/enums/community.enum';
+import { lookUpMember } from '../../libs/config';
+import { T } from '../../libs/types/common';
 
 @Injectable()
 export class CommentService {
@@ -24,7 +31,7 @@ export class CommentService {
     private viewService: ViewService,
     private productService: ProductService,
   ) {}
-
+  //createComment
   public async createComment(
     memberId: ObjectId,
     input: CommentInput,
@@ -63,5 +70,59 @@ export class CommentService {
     }
     if (!result) throw new InternalServerErrorException(Message.CREATE_FAILED);
     return result;
+  }
+  //updateComment
+  public async updateComment(
+    memberId: ObjectId,
+    input: CommentUpdate,
+  ): Promise<Comment> {
+    const result = await this.commentModel.findOneAndUpdate(
+      {
+        _id: input._id,
+        memberId: memberId,
+        commentStatus: CommentStatus.ACTIVE,
+      },
+      input,
+      { new: true },
+    );
+    if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
+    return result;
+  }
+  //getComments
+  public async getComments(
+    memberId: ObjectId,
+    input: CommentsInquiry,
+  ): Promise<Comments> {
+    const { commentRefId } = input.search;
+    const match: T = {
+      commentRefId: commentRefId,
+      commentStatus: CommentStatus.ACTIVE,
+    };
+    const sort: T = {
+      [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC,
+    };
+
+    const result = await this.commentModel
+      .aggregate([
+        { $match: match },
+        { $sort: sort },
+        {
+          $facet: {
+            list: [
+              { $skip: (input.page - 1) * input.limit },
+              { $limit: input.limit },
+              //liked
+              lookUpMember,
+              { $unwind: '$memberData' },
+            ],
+            metaCounter: [{ $count: 'total' }],
+          },
+        },
+      ])
+      .exec();
+    if (!result.length)
+      throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+    return result[0];
   }
 }
