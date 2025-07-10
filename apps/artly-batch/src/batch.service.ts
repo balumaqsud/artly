a99e37197d154c6abb2505188c5a0ae6e93b7466
balumaqsud <ollobergan008@mail.ru>
2025-07-10 18:08:50 +0900

    feat: modify batch module, and database connection

--- apps/artly-batch/src/batch.service.ts
+++ apps/artly-batch/src/batch.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ArtlyBatchService {
+export class BatchService {
   getHello(): string {
     return 'Hello World! artly batch api';
   }
--- apps/artly-batch/test/app.e2e-spec.ts
+++ apps/artly-batch/test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { ArtlyBatchModule } from './../src/artly-batch.module';
+import { ArtlyBatchModule } from '../src/batch.module';
 
 describe('ArtlyBatchController (e2e)', () => {
   let app: INestApplication;
