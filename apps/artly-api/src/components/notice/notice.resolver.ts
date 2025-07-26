2cc81d8037593f3d038a4c5a6d709bb3a7dc9e70
balumaqsud <ollobergan008@mail.ru>
2025-07-26 23:50:27 +0900

    fix: get and update NOtice

--- apps/artly-api/src/components/notice/notice.resolver.ts
+++ apps/artly-api/src/components/notice/notice.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { NoticeService } from './notice.service';
 import { Notice } from '../../libs/dto/notice/notice';
 import { Roles } from '../auth/decorators/roles.decorator';
@@ -8,6 +8,9 @@ import { UseGuards } from '@nestjs/common';
 import { ObjectId } from 'mongoose';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
 import { NoticeInput } from '../../libs/dto/notice/notice.input';
+import { shapeId } from '../../libs/config';
+import { WithoutGuard } from '../auth/guards/without.guard';
+import { NoticeUpdate } from '../../libs/dto/notice/notice.update';
 
 @Resolver()
 export class NoticeResolver {
@@ -22,5 +25,28 @@ export class NoticeResolver {
   ): Promise<Notice> {
     console.log('mutation, create article');
     return await this.noticeService.createNotice(memberId, input);
+  }
+
+  @UseGuards(WithoutGuard)
+  @Query((returns) => Notice)
+  public async getNotice(
+    @Args('input') input: String,
+    @AuthMember('_id') memberId: ObjectId,
+  ): Promise<Notice> {
+    console.log('query, get notice');
+    const noticeId = shapeId(input);
+    return await this.noticeService.getNotice(memberId, noticeId);
+  }
+
+  @Roles(MemberType.ADMIN)
+  @UseGuards(RolesGuard)
+  @Mutation((returns) => Notice)
+  public async updateNotice(
+    @Args('input') input: NoticeUpdate,
+    @AuthMember('_id') memberId: ObjectId,
+  ): Promise<Notice> {
+    console.log('mutation, updateNotice');
+    input._id = shapeId(input._id);
+    return await this.noticeService.updateNotice(memberId, input);
   }
 }
--- apps/artly-api/src/components/notice/notice.service.ts
+++ apps/artly-api/src/components/notice/notice.service.ts
@@ -1,10 +1,16 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Notice } from '../../libs/dto/notice/notice';
 import { Model, ObjectId } from 'mongoose';
 import { MemberService } from '../member/member.service';
 import { NoticeInput } from '../../libs/dto/notice/notice.input';
 import { Message } from '../../libs/enums/common.enum';
+import { NoticeStatus } from '../../libs/enums/notice.enum';
+import { NoticeUpdate } from '../../libs/dto/notice/notice.update';
 
 @Injectable()
 export class NoticeService {
@@ -26,5 +32,34 @@ export class NoticeService {
       console.log('create error', error);
       throw new BadRequestException(Message.CREATE_FAILED);
     }
+  }
+
+  public async getNotice(
+    memberId: ObjectId,
+    noticeId: ObjectId,
+  ): Promise<Notice> {
+    const search = { _id: noticeId, noticeStatus: NoticeStatus.HOLD };
+    const result = await this.noticeModel.findOne(search).exec();
+
+    if (!result) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+    return result;
+  }
+
+  public async updateNotice(
+    memberId: ObjectId,
+    input: NoticeUpdate,
+  ): Promise<Notice> {
+    const match = {
+      _id: input._id,
+      memberId: memberId,
+    };
+
+    const result = await this.noticeModel
+      .findOneAndUpdate(match, input, { new: true })
+      .exec();
+    if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
+
+    return result;
   }
 }
